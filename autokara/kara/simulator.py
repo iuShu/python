b60f23f09cbd463cc1222546f65cd348903fdd9c
iuShu <13622409484@163.com>
2022-03-02 01:11:20 +0800

    critical solution

--- autokara/kara/simulator.py
+++ autokara/kara/simulator.py
@@ -1,3 +1,5 @@
+import win32api
+import win32con
 import win32gui
 from PIL import ImageGrab
 
@@ -16,3 +18,22 @@ class Simulator(object):
     def click(self, p):
         pass
 
+
+def test():
+    """
+    see https://zhuanlan.zhihu.com/p/309664632
+    """
+    hwnd = win32gui.FindWindow(0, '雷电模拟器')
+    child = win32gui.GetWindow(hwnd, win32con.GW_CHILD)
+    # handle = win32gui.WindowFromPoint((100, 100))
+    # print(hwnd, child, handle)
+    raw = (130, 150)
+    pos = win32gui.ScreenToClient(child, raw)
+    wp = win32api.MAKELONG(pos[0], pos[1])
+    # win32gui.SendMessage(child, win32con.WM_ACTIVATE, win32con.WA_ACTIVE, 0)
+    win32gui.SendMessage(child, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, wp)
+    win32gui.SendMessage(child, win32con.WM_LBUTTONUP, win32con.MK_LBUTTON, wp)
+
+
+if __name__ == '__main__':
+    test()
