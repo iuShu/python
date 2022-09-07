56b9c12752df3289aaacdc561a25f24c9374f1d5
iuShu <13622409484@163.com>
2022-09-07 17:51:43 +0800

    daily update

--- okx-trade/src/config.py
+++ okx-trade/src/config.py
@@ -16,6 +16,13 @@ def db_conf(db_type: str):
         return raw[db_type]
 
 
+def notify_conf(name: str):
+    loc = os.path.join(os.path.dirname(__file__), '../resources/notify.json')
+    with open(loc, 'r', encoding='utf-8') as f:
+        raw = json.load(fp=f)
+        return raw[name]
+
+
 if __name__ == '__main__':
     # print(load_config())
     print(conf('okx'))
---
+++ okx-trade/src/martin/server/ws.py
@@ -0,0 +1,42 @@
+import time
+
+from aiohttp import web, WSMsgType, WSMessage
+from aiohttp.web_request import Request
+
+routes = web.RouteTableDef()
+channels = []
+
+
+@routes.get('/')
+async def hello(request: Request):
+    resp = {
+        'code': 200,
+        'msg': 'success',
+        'data': 'hello ' + str(int(time.time()))
+    }
+    return web.json_response(data=resp)
+
+
+@routes.get('/ws')
+async def handle_req(request: Request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+    channels.append(ws)
+    async for msg in ws:
+        msg: WSMessage = msg
+        if msg.type == WSMsgType.TEXT:
+            resp = {
+                'code': 200,
+                'msg': 'success',
+                'data': 'recv ' + msg.data
+            }
+            await ws.send_json(resp)
+            if msg.data == 'wss-close':
+                await ws.close()
+    print('wss connection closed')
+    return ws
+
+
+app = web.Application()
+app.add_routes(routes)
+web.run_app(app)
